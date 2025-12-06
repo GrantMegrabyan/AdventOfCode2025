c32056adb5c04e9dfc3ba7f5afb53a1e38eee554
Grant Megrabyan <grant.megrabyan@gmail.com>
2025-12-06 10:49:15 +0000

    Day 05, part 2

--- src/day05.rs
+++ src/day05.rs
@@ -22,6 +22,38 @@ pub fn part1(input: &str) -> Result<u64> {
     Ok(fresh_ids.len() as u64)
 }
 
+pub fn part2(input: &str) -> Result<u64> {
+    let (ranges, _) = input.split_once("\n\n").unwrap();
+
+    let mut ranges = ranges
+        .lines()
+        .map(|line| line.split_once("-").unwrap())
+        .map(|(start, end)| {
+            let start = start.parse::<u64>().unwrap();
+            let end = end.parse::<u64>().unwrap();
+            (start, end)
+        })
+        .collect::<Vec<_>>();
+    ranges.sort_by_key(|(start, _)| *start);
+    let valid_ids: u64 = ranges
+        .iter()
+        .fold(vec![], |mut acc: Vec<(u64, u64)>, (start, end)| {
+            if let Some(last) = acc.last_mut()
+                && last.1 >= *start
+            {
+                last.1 = last.1.max(*end);
+            } else {
+                acc.push((*start, *end));
+            }
+            acc
+        })
+        .iter()
+        .map(|(start, end)| *end - *start + 1)
+        .sum();
+
+    Ok(valid_ids)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -41,5 +73,10 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1(INPUT).unwrap(), 3);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT).unwrap(), 14);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -58,6 +58,10 @@ fn run(day: &str, part: &str, input: &str) {
             let result = day05::part1(input).unwrap();
             println!("Result: {}", result);
         }
+        ("day05", "part2") => {
+            let result = day05::part2(input).unwrap();
+            println!("Result: {}", result);
+        }
         _ => panic!("Invalid day or part"),
     }
 }
